dcc0d1628c40d64fecb22a8eccd545690ab4c5a0
Chance Snow <git@chancesnow.me>
2025-12-31 19:14:42 -0600

    Refactor example framework

--- examples/example/Example.cs
+++ examples/example/Example.cs
@@ -5,90 +5,82 @@ using Veldrid;
 using Veldrid.Sdl2;
 using Veldrid.StartupUtilities;
 
-namespace Teraflop.Examples
-{
-    public abstract class Example : Game
-    {
-        private Sdl2Window _window;
+namespace Teraflop.Examples {
+	public abstract class Example : Game {
+		private Sdl2Window _window;
 
-        public Example()
-        {
-            AssetSources.Add(new AssemblyAssetSource());
-        }
+		public Example() {
+			AssetSources.Add(new AssemblyAssetSource());
+		}
 
-        public string Title { get; set; } = "Example";
-        public bool DebugMode { get; set; } = true;
+		public string Title { get; set; } = "Example";
+		public bool DebugMode { get; set; } = true;
 
-        protected override GraphicsDevice CreateGraphicsDevice()
-        {
-            // TODO: Migrate this to Sdl2Native.SDL_WINDOWPOS_CENTERED
-            const int windowPositionCentered = 0x2FFF0000;
-            var windowCreateInfo = new WindowCreateInfo
-            {
-                X = windowPositionCentered,
-                Y = windowPositionCentered,
-                WindowWidth = 960,
-                WindowHeight = 540,
-                WindowTitle = Title
-            };
-            _window = VeldridStartup.CreateWindow(ref windowCreateInfo);
-            _window.Resized += () => _framebufferSizeProvider.Update(_window.Width, _window.Height);
-            // _window.Closing += Exit; // TODO: Save game infrastructure
-            _window.Closed += Exit;
-            _window.CursorVisible = true;
+		protected override GraphicsDevice CreateGraphicsDevice() {
+			// TODO: Migrate this to Sdl2Native.SDL_WINDOWPOS_CENTERED
+			const int windowPositionCentered = 0x2FFF0000;
+			var windowCreateInfo = new WindowCreateInfo {
+				X = windowPositionCentered,
+				Y = windowPositionCentered,
+				WindowWidth = 960,
+				WindowHeight = 540,
+				WindowTitle = Title
+			};
+			_window = VeldridStartup.CreateWindow(ref windowCreateInfo);
+			_window.Resized += () => _framebufferSizeProvider.Update(_window.Width, _window.Height);
+			// _window.Closing += Exit; // TODO: Save game infrastructure
+			_window.Closed += Exit;
+			_window.CursorVisible = true;
 
-            // TODO: Setup multisampling AA
+			// TODO: Setup multisampling AA
 
-            var options = new GraphicsDeviceOptions(DebugMode)
-            {
-                SwapchainDepthFormat = PixelFormat.R16_UNorm,
-                PreferDepthRangeZeroToOne = true,
-                PreferStandardClipSpaceYDirection = true
-            };
+			var options = new GraphicsDeviceOptions(DebugMode) {
+				SwapchainDepthFormat = PixelFormat.R16_UNorm,
+				PreferDepthRangeZeroToOne = true,
+				PreferStandardClipSpaceYDirection = true
+			};
 
-            var isMacOs = RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
-            var isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
-            var isLinux = RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
-            var defaultBackend = VeldridStartup.GetPlatformDefaultBackend();
-            bool isVulkanSupported = GraphicsDevice.IsBackendSupported(GraphicsBackend.Vulkan);
-            var isVulkanAvailable = (isWindows && isVulkanSupported) ||
-                (isLinux && defaultBackend == GraphicsBackend.Vulkan);
-            var device = isVulkanAvailable
-                ? VeldridStartup.CreateVulkanGraphicsDevice(options, _window)
-                : isWindows
-                    ? VeldridStartup.CreateGraphicsDevice(_window, options)
-                    : VeldridStartup.CreateDefaultOpenGLGraphicsDevice(options, _window, GraphicsBackend.OpenGL);
+			var isMacOs = RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+			var isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+			var isLinux = RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+			var defaultBackend = VeldridStartup.GetPlatformDefaultBackend();
+			bool isVulkanSupported = GraphicsDevice.IsBackendSupported(GraphicsBackend.Vulkan);
+			var isVulkanAvailable = (isWindows && isVulkanSupported) ||
+				(isLinux && defaultBackend == GraphicsBackend.Vulkan);
+			var device = isVulkanAvailable
+				? VeldridStartup.CreateVulkanGraphicsDevice(options, _window)
+				: isWindows
+					? VeldridStartup.CreateGraphicsDevice(_window, options)
+					: VeldridStartup.CreateDefaultOpenGLGraphicsDevice(options, _window, GraphicsBackend.OpenGL);
 
-            return device;
-        }
+			return device;
+		}
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        protected override void Update(GameTime gameTime)
-        {
-            if (!_window.Exists)
-            {
-                Exit();
-                return;
-            }
+		/// <summary>
+		/// Allows the game to run logic such as updating the world,
+		/// checking for collisions, gathering input, and playing audio.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Update(GameTime gameTime) {
+			if (!_window.Exists) {
+				Exit();
+				return;
+			}
 
-            var inputSnapshot = _window.PumpEvents();
-            ProcessInput(InputSnapshotConverter.Mouse(inputSnapshot, MouseState),
-                InputSnapshotConverter.Keyboard(inputSnapshot));
+			var inputSnapshot = _window.PumpEvents();
+			ProcessInput(InputSnapshotConverter.Mouse(inputSnapshot, MouseState),
+				InputSnapshotConverter.Keyboard(inputSnapshot));
 
-            if (KeyboardState.IsKeyDown(Input.Key.Escape))
-                Exit();
+			if (KeyboardState.IsKeyDown(Input.Key.Escape))
+				Exit();
 
-            base.Update(gameTime);
+			base.Update(gameTime);
 
-            if (DebugMode) {
-                var backend = GraphicsDevice.BackendType;
-                var frameTime = Math.Round(gameTime.ElapsedGameTime.TotalMilliseconds, 1);
-                _window.Title = $"{Title} - {backend} - {frameTime} ms - {FramesPerSecond} fps";
-            }
-        }
-    }
+			if (DebugMode) {
+				var backend = GraphicsDevice.BackendType;
+				var frameTime = Math.Round(gameTime.ElapsedGameTime.TotalMilliseconds, 1);
+				_window.Title = $"{Title} - {backend} - {frameTime} ms - {FramesPerSecond} fps";
+			}
+		}
+	}
 }
--- examples/example/InputSnapshotConverter.cs
+++ examples/example/InputSnapshotConverter.cs
@@ -6,38 +6,34 @@ using Veldrid;
 using Key = Teraflop.Input.Key;
 using MouseButton = Teraflop.Input.MouseButton;
 
-namespace Teraflop.Examples
-{
-    public static class InputSnapshotConverter
-    {
-        public static MouseState Mouse(InputSnapshot snapshot, MouseState previousMouseState)
-        {
-            var downButtons = snapshot.MouseEvents.Where(m => m.Down).Select(m => MouseButton(m.MouseButton))
-                .ToImmutableList();
-            var upButtons = snapshot.MouseEvents.Where(m => !m.Down).Select(m => MouseButton(m.MouseButton))
-                .ToImmutableList();
-            var wheelValue = (int) Math.Round(snapshot.WheelDelta);
+namespace Teraflop.Examples {
+	public static class InputSnapshotConverter {
+		public static MouseState Mouse(InputSnapshot snapshot, MouseState previousMouseState) {
+			var downButtons = snapshot.MouseEvents.Where(m => m.Down).Select(m => MouseButton(m.MouseButton))
+				.ToImmutableList();
+			var upButtons = snapshot.MouseEvents.Where(m => !m.Down).Select(m => MouseButton(m.MouseButton))
+				.ToImmutableList();
+			var wheelValue = (int)Math.Round(snapshot.WheelDelta);
 
-            return new MouseState(snapshot.MousePosition, downButtons, upButtons, wheelValue, previousMouseState);
-        }
+			return new MouseState(snapshot.MousePosition, downButtons, upButtons, wheelValue, previousMouseState);
+		}
 
-        private static MouseButton MouseButton(Veldrid.MouseButton button) => (MouseButton) (int) button;
+		private static MouseButton MouseButton(Veldrid.MouseButton button) => (MouseButton)(int)button;
 
-        public static KeyboardState Keyboard(InputSnapshot snapshot)
-        {
-            var downKeys = snapshot.KeyEvents.Where(key => key.Down).Select(key => Key(key.Key)).ToImmutableList();
-            var upKeys = snapshot.KeyEvents.Where(key => !key.Down).Select(key => Key(key.Key)).ToImmutableList();
-            var keyModifiers = snapshot.KeyEvents.Where(key => key.Down)
-                .Select(key => key.Modifiers)
-                .Aggregate(Teraflop.Input.KeyboardModifiers.None,
-                    (modifiers, keys) => modifiers | KeyboardModifiers(keys));
+		public static KeyboardState Keyboard(InputSnapshot snapshot) {
+			var downKeys = snapshot.KeyEvents.Where(key => key.Down).Select(key => Key(key.Key)).ToImmutableList();
+			var upKeys = snapshot.KeyEvents.Where(key => !key.Down).Select(key => Key(key.Key)).ToImmutableList();
+			var keyModifiers = snapshot.KeyEvents.Where(key => key.Down)
+				.Select(key => key.Modifiers)
+				.Aggregate(Teraflop.Input.KeyboardModifiers.None,
+					(modifiers, keys) => modifiers | KeyboardModifiers(keys));
 
-            return new KeyboardState(downKeys, upKeys, keyModifiers);
-        }
+			return new KeyboardState(downKeys, upKeys, keyModifiers);
+		}
 
-        private static Key Key(Veldrid.Key key) => (Key) (int) key;
+		private static Key Key(Veldrid.Key key) => (Key)(int)key;
 
-        private static KeyboardModifiers KeyboardModifiers(ModifierKeys modifierKeys) =>
-            (KeyboardModifiers) (int) modifierKeys;
-    }
+		private static KeyboardModifiers KeyboardModifiers(ModifierKeys modifierKeys) =>
+			(KeyboardModifiers)(int)modifierKeys;
+	}
 }

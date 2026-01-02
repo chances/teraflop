5b191f64bbf845e39e48f3b51a4b4fbe7da038e6
Chance Snow <git@chancesnow.me>
2026-01-01 23:01:13 -0600

    Add a 2D plane primitive

---
+++ Teraflop/Primitives/Plane.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.Numerics;
+using Teraflop.Buffers;
+using Teraflop.Buffers.Layouts;
+using Teraflop.Components;
+
+namespace Teraflop.Primitives {
+    public class Plane : IPrimitive {
+        public Plane(string name) {
+            var mesh = new MeshBuilder().WithVertices(new List<IVertexBufferDescription> {
+                new VertexPositionNormalTexture(new Vector3(-1, -1, 0), new Vector3(0, 0, 1), new Vector2(0, 0)),
+                new VertexPositionNormalTexture(new Vector3(-1, 1, 0), new Vector3(0, 0, 1), new Vector2(0, 1)),
+                new VertexPositionNormalTexture(new Vector3(1, 1, 0), new Vector3(0, 0, 1), new Vector2(1, 1)),
+                new VertexPositionNormalTexture(new Vector3(1, -1, 0), new Vector3(0, 0, 1), new Vector2(1, 0))
+			}).WithIndices(new ushort[] { 0, 1, 2, 0, 2, 3 });
+
+            MeshData = mesh.Build<VertexPositionNormalTexture>(name);
+        }
+
+        public MeshData MeshData { get; private set; }
+    }
+}
